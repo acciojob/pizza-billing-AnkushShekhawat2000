cc677c2e4b0f1ab5d9332c3a0d9f3e7ff9be4878
Ashekhawat@2001 <ankush75056@gmail.com>
2023-08-22 15:30:58 +0530

    1

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -45,7 +45,7 @@ public class Pizza {
         isPapperBagAdded = false;
 
         totalPrice = basePrice;
-        bill = "Base Price of The Pizza: "+basePrice + "\n";
+        bill = "Base Price Of The Pizza: "+basePrice + "\n";
 
     }
 
